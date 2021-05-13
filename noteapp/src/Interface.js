a8c4168bcb2bea9aff5f863614afe826ef6280a5
Mike Jeuga <mikejeuga@gmail.com>
2021-05-13 14:54:22 +0100

    Emoji clean

--- noteapp/src/Interface.js
+++ noteapp/src/Interface.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
         currentNote.textContent = noteapp.show20Chars(noteapp.list.length)
         document.querySelector("ol").append(currentNote)
         document.querySelector(".note-import").value = ""
-    })        // console.log(createEmoji(note))
+    })
 
 
 
@@ -26,29 +26,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
         document.querySelectorAll("li")[noteNumber - 1].innerText = expandedNote
     })
-
-// showDogPic = async() => {
-    //     let dogSrc = await fetch("https://dog.ceo/api/breeds/image/random")
-    //     .then(res => res.json()).then(myPhoto => myPhoto.message)
-
-    //     let thePic = document.createElement("img")                   
-    //     thePic.src = dogSrc
-    //     document.querySelector("ol").append(thePic)
-    // }
-
-    // showDogPic()
-    createEmoji = async(text) => {
-       let jsonText = JSON.parse(`{ "text": "${text}" }`)
-        let response = await fetch('https://makers-emojify.herokuapp.com/', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(jsonText)
-            })
-        let data = response.json().emojified_text
-        return data
-    }
 
     createEmoji = (text) => {
         let jsonText = JSON.parse(`{ "text": "${text}" }`)
